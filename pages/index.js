da8f35066f89535e6017b1a294528c3d7a9de629
Joshuamjv2 <joshuamjv22@gmail.com>
2023-07-19 15:52:30 +0300

    unknown

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Navigation from '../components/Navigation/Navigation'
-import Footer from '@/components/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
