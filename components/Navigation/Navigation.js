fe122426c59123d438c9c3a7c4a20c9d1ae38962
Joshuamjv2 <joshuamjv22@gmail.com>
2023-10-21 08:07:47 +0300

    fix name

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -66,7 +66,7 @@ export default function Navigation(){
                 <div className="container flex items-center h-full">
                     <div>
                         <h5 className="mb-2 text-[#6A040F] md:text-xl xl:text-2xl">Hello there, my name is</h5>
-                        <h1 className="uppercase font-bold text-[#264653] text-3xl md:text-5xl lg:text-6xl">Muwanguzi Joshua.</h1>
+                        <h1 className="uppercase font-bold text-[#264653] text-3xl md:text-5xl lg:text-6xl">Muwanguzi Joshua</h1>
                         <h2 className="text-[#3c6e71] py-2 xl:py-4 text-2xl md:text-4xl xl:text-5xl font-bold">I build software for the web.</h2>
                         <p className="text-[#2b2d42] xl:w-1/2 lg:text-base">I am a software engineer specialized in building for the web both on the backend and frontend, with more proficiency in the backend.
                         I am currently working as a python backend developer at an Italian based start up called <span className="text-[#6a040f] font-bold"><a href="https://www.pelrio.com" target="_blank">Pelrio</a></span>, building tools that automate accounting processes for small businesses.</p>
